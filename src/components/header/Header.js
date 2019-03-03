989a86156e5674f1d68fb8c34f3102a45167710e
naneva <naneva140156@gmail.com>
2019-03-03 10:23:50 +0200

    new

---
+++ src/components/header/Header.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="warning" light expand="md">
+          <NavbarBrand href="/">VeRa - образователна система </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Моят профил</NavLink>
+              </NavItem>
+
+              <NavItem>
+                <NavLink href="/components/">Ученици</NavLink>
+              </NavItem>
+              
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Домашни и уроци   
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Виж домашни
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Виж уроци 
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Добави домашна
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Добави урок
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    История домашни 
+                  </DropdownItem>
+                  <DropdownItem>
+                    История уроци 
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+
+               <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Тестове 
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Виж тестове
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Добави тестове
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    История 
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+
+               <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Настройки  
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Смени тема 
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Смени шрифт 
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
---
+++ src/components/home/Home.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Container, Row, Col,
+        Card, CardImg, CardText, CardBody,
+        CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Container >
+            <Row>
+                <Col>
+                    <p class="mt-3" > Предстоящи тестове  </p><hr/>
+                    <Row> 
+                        <Col>
+                            <Card>
+                                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                <CardBody>
+                                <CardTitle>Въведение в компютърните науки</CardTitle>
+                                <CardSubtitle> 20/03/2019</CardSubtitle>
+                                <CardText> За 5-7 клас </CardText>
+                                <Button color="warning">Виж</Button>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                <CardBody>
+                                <CardTitle>Въведение в компютърните науки</CardTitle>
+                                <CardSubtitle> 20/03/2019</CardSubtitle>
+                                <CardText> За 5-7 клас </CardText>
+                                <Button color="warning">Виж</Button>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                    
+                </Col>
+                <Col>
+                    <p class="mt-3" > Последни уроци   </p> <hr/>
+                    <Row> 
+                        <Col>
+                            <Card>
+                                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                <CardBody>
+                                <CardTitle>Въведение в компютърните науки</CardTitle>
+                                <CardSubtitle> 20/03/2019</CardSubtitle>
+                                <CardText> За 5-7 клас </CardText>
+                                <Button color="warning" >Виж </Button>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                                <CardBody>
+                                <CardTitle>Въведение в компютърните науки</CardTitle>
+                                <CardSubtitle> 20/03/2019</CardSubtitle>
+                                <CardText> За 5-7 клас </CardText>
+                                <Button color="warning" >Виж</Button>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
+      </div>
+    );
+  }
+}
